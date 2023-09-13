068b30edaa8e7603ab866054081f1efad48e5086
Ryan <73886836+ryanvaugarni@users.noreply.github.com>
2023-09-13 17:20:38 +0200

    update all

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log("Vous êtes connecté")
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Vous êtes connecté");
     let date, foot
     date = new Date();
     foot = document.querySelector("footer p");
     foot.innerText += `${date.getFullYear()} - Ryan Vaugarni | Tous droits réservés`
 
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+    $('.navTrigger').click(function () {
+        $(this).toggleClass('active');
+        console.log("Clicked menu");
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
 
-});
-})
+    });
+        $(window).scroll(function () {
+            if ($(document).scrollTop() > 50) {
+                $('nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('nav').removeClass('affix');
+            }
+        });
+
+    });
